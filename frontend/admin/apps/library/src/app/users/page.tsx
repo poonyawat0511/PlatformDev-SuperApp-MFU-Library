11f4a9b3eca21b48b9c0550c0942970dc44a2901
poonyawat0931 <117950023+poonyawat0511@users.noreply.github.com>
2024-11-05 06:06:20 +0700

    frontend: Library: improved user page 1.1

--- frontend/admin/apps/library/src/app/users/page.tsx
+++ frontend/admin/apps/library/src/app/users/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import ConfirmDialog from "@/components/Users/ConfirmDialog";
+import * as Icons from "@heroicons/react/24/outline";
+import { useGlobalContext } from "@shared/context/GlobalContext";
+import { tAlert, tAlertType } from "@shared/utils/types/Alert";
 import { useEffect, useState } from "react";
-import { User } from "../../utils/UserTypes";
 import UserForm from "../../components/Users/UserForm";
 import UserTable from "../../components/Users/UserTable";
+import { User } from "../../utils/UserTypes";
 
 const apiUrl = "http://localhost:8082/api/users";
 
@@ -26,43 +30,76 @@ const fetchUsers = async (): Promise<User[]> => {
 };
 
 export default function UserPage() {
+  const { addAlert } = useGlobalContext();
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const [editingUser, setEditingUser] = useState<User | null>(null);
   const [isFormOpen, setIsFormOpen] = useState<boolean>(false);
+  const [isConfirmDialogOpen, setIsConfirmDialogOpen] = useState(false);
+  const [userIdToDelete, setUserIdToDelete] = useState<string | null>(
+    null
+  );
+  const [selectedUser, setSelectedUser] = useState<User | null>(
+    null
+  );
+  const handleAddAlert = (
+    iconName: keyof typeof Icons,
+    title: string,
+    message: string,
+    type: tAlertType
+  ) => {
+    const newAlert: tAlert = {
+      title: title,
+      message: message,
+      buttonText: "X",
+      iconName: iconName,
+      type: type,
+      id: Math.random().toString(36).substring(2, 9),
+    };
+    addAlert(newAlert);
+  };
 
   useEffect(() => {
-    fetchUsers().then((data) => {
-      setUsers(data);
+    const fetchData = async () => {
+      const fetchedUsers = await fetchUsers();
+      setUsers(fetchedUsers);
       setLoading(false);
-    });
+    };
+
+    fetchData();
   }, []);
 
   const handleCreate = () => {
-    setEditingUser(null);
+    setSelectedUser(null);
     setIsFormOpen(true);
   };
 
   const handleEdit = (user: User) => {
-    setEditingUser(user);
+    setSelectedUser(user);
     setIsFormOpen(true);
   };
 
-  const handleDelete = async (userId: string) => {
-    if (confirm("Are you sure you want to delete this User?")) {
-      try {
-        const response = await fetch(`${apiUrl}/${userId}`, {
-          method: "DELETE",
-        });
-        if (response.ok) {
-          setUsers(users.filter((user) => user.id !== userId));
-        } else {
-          const errorText = await response.text();
-          console.error(`Failed to delete user: ${errorText}`);
-        }
-      } catch (error) {
-        console.error("Failed to delete user:", error);
+  const confirmDelete = (userId: string) => {
+    setUserIdToDelete(userId);
+    setIsConfirmDialogOpen(true);
+  };
+
+  const handleDelete = async () => {
+    if (!userIdToDelete) return;
+  
+    try {
+      const response = await fetch(`${apiUrl}/${userIdToDelete}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Failed to delete timeslot");
       }
+      setUsers(users.filter((t) => t.id !== userIdToDelete));
+      handleAddAlert("ExclamationCircleIcon", "Success", "Transaction deleted successfully", tAlertType.SUCCESS);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsConfirmDialogOpen(false);
+      setUserIdToDelete(null);
     }
   };
 
@@ -104,6 +141,12 @@ export default function UserPage() {
           }
         }
         setIsFormOpen(false);
+        handleAddAlert(
+          "ExclamationCircleIcon",
+          "Success",
+          "User updated successfully",
+          tAlertType.SUCCESS
+        );
       } else {
         const errorText = await response.text();
         console.error(`Failed to submit user: ${errorText}`);
@@ -118,36 +161,45 @@ export default function UserPage() {
   }
 
   return (
-    <div>
-      <button
-        onClick={handleCreate}
-        className="bg-blue-500 text-white px-4 py-2 rounded mb-4"
-      >
-        Create New User
-      </button>
-      <div className="flex flex-wrap justify-start">
-        {users && users.length > 0 ? (
-          <div className="flex flex-wrap justify-start">
-            {users.map((user) => (
-              <UserTable
-                key={user.id}
-                user={user}
-                onEdit={handleEdit}
-                onDelete={handleDelete}
+    <div className="min-h-screen p-6">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center mb-4 px-4 border-b-2">
+          <h1 className="text-3xl font-bold mb-6 text-gray-800">
+            User
+          </h1>
+          <button
+            onClick={handleCreate}
+            className="bg-black text-white px-4 py-2 rounded-full shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-white mb-6"
+          >
+            New User
+          </button>
+        </div>
+        <div className="flex flex-wrap justify-start">
+          <UserTable
+            users={users}
+            onEdit={handleEdit}
+            onDelete={confirmDelete}
+          />
+        </div>
+
+        {isFormOpen && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg">
+              <UserForm
+                user={selectedUser}
+                onSubmit={handleFormSubmit}
+                onClose={() => setIsFormOpen(false)}
               />
-            ))}
+            </div>
           </div>
-        ) : (
-          <div>No users found</div>
         )}
       </div>
-      {isFormOpen && (
-        <UserForm
-          user={editingUser}
-          onSubmit={handleFormSubmit}
-          onClose={() => setIsFormOpen(false)}
-        />
-      )}
+      <ConfirmDialog
+        isOpen={isConfirmDialogOpen}
+        onConfirm={handleDelete}
+        onClose={() => setIsConfirmDialogOpen(false)}
+        message="Are you sure you want to delete this time slot?"
+      />
     </div>
   );
 }
--- frontend/admin/apps/library/src/components/Users/UserForm.tsx
+++ frontend/admin/apps/library/src/components/Users/UserForm.tsx
@@ -1,100 +1,182 @@
 import { User } from "@/utils/UserTypes";
-import { useEffect, useState } from "react";
+import * as Icons from "@heroicons/react/24/outline";
+import Modal from "@shared/components/Modal";
+import { useGlobalContext } from "@shared/context/GlobalContext";
+import { tAlert, tAlertType } from "@shared/utils/types/Alert";
+import React, { useEffect, useState } from "react";
+import { GrFormClose } from "react-icons/gr";
+import { LiaCheckCircle } from "react-icons/lia";
 
 interface UserFormProps {
-  user?: User | null;
-  onSubmit: (data: User) => void;
+  user: User | null;
+  onSubmit: (formData: User) => Promise<void>;
   onClose: () => void;
 }
 
-export default function UserForm({ user, onSubmit, onClose }: UserFormProps) {
-  const [email, setEmail] = useState<string>("");
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+const UserForm: React.FC<UserFormProps> = ({ user, onSubmit, onClose }) => {
+  const [formData, setFormData] = useState<User>({
+    id: user?.id || "",
+    username: user?.username || "",
+    email: user?.email || "",
+    password: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const { addAlert } = useGlobalContext();
+  const handleAddAlert = (
+    iconName: keyof typeof Icons,
+    title: string,
+    message: string,
+    type: tAlertType
+  ) => {
+    const newAlert: tAlert = {
+      title: title,
+      message: message,
+      buttonText: "X",
+      iconName: iconName,
+      type: type,
+      id: Math.random().toString(36).substring(2, 9),
+    };
+    addAlert(newAlert);
+  };
 
   useEffect(() => {
     if (user) {
-      setEmail(user.email);
-      setUsername(user.username);
-      setPassword(user.password);
+      setFormData({
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        password: "",
+      });
+    } else {
+      setFormData({
+        id: "",
+        username: "",
+        email: "",
+        password: "",
+      });
     }
   }, [user]);
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
+    setIsSubmitting(true);
+    setError(null);
 
-    const data: User = {
-      id: user?.id || "",
-      email,
-      username,
-      password,
-    };
+    if (!formData.username) {
+      handleAddAlert(
+        "ExclamationCircleIcon",
+        "Username Missing",
+        "Username is required",
+        tAlertType.WARNING
+      );
+      setIsSubmitting(false);
+      return;
+    }
 
-    onSubmit(data);
+    if (!formData.email) {
+      handleAddAlert(
+        "ExclamationCircleIcon",
+        "Email Missing",
+        "Email is required",
+        tAlertType.WARNING
+      );
+      setIsSubmitting(false);
+      return;
+    }
+
+    try {
+      await onSubmit({
+        ...formData,
+        id: formData.id || undefined,
+      });
+      setFormData({
+        id: "",
+        username: "",
+        email: "",
+        password: "",
+      });
+      onClose();
+    } catch (error) {
+      setError("Failed to submit user. Please check the form inputs.");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target;
+
+    setFormData({ ...formData, [name]: value });
   };
 
   return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white p-6 rounded shadow-lg w-full max-w-md">
-        <h2 className="text-xl font-bold mb-4">
-          {user ? "Edit User" : "Create User"}
-        </h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Email
-            </label>
-            <input
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Username
-            </label>
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              required
-            />
-          </div>
+    <Modal
+      isOpen={true}
+      title={user ? "Edit Form" : "Create Form"}
+      onClose={onClose}
+      actions={
+        <div className="flex justify-between">
+          <button
+            onClick={handleSubmit}
+            disabled={isSubmitting}
+            className="bg-black text-white px-2 py-2 rounded-full"
+          >
+            <LiaCheckCircle className="size-6" />
+          </button>
+          <button
+            onClick={onClose}
+            className="bg-gray-500 text-white px-2 py-2 rounded-full ml-6"
+          >
+            <GrFormClose className="size-6" />
+          </button>
+        </div>
+      }
+    >
+      {error && <p className="text-red-500">{error}</p>}
 
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Password
-            </label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              required
-            />
-          </div>
-
-          <div className="flex justify-end">
-            <button
-              type="button"
-              onClick={onClose}
-              className="bg-gray-500 text-white px-4 py-2 rounded mr-2"
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="bg-blue-500 text-white px-4 py-2 rounded"
-            >
-              {user ? "Update" : "Create"}
-            </button>
-          </div>
-        </form>
+      <div className="space-y-2">
+        <label className="block text-gray-700 font-medium">Username</label>
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          placeholder="Enter username"
+          required
+          className="w-full p-2 border border-gray-300 rounded-lg focus:ring focus:ring-blue-200 focus:border-blue-500"
+        />
       </div>
-    </div>
+
+      <div className="space-y-2">
+        <label className="block text-gray-700 font-medium">Email</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Enter Email"
+          required
+          className="w-full p-2 border border-gray-300 rounded-lg focus:ring focus:ring-blue-200 focus:border-blue-500"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <label className="block text-gray-700 font-medium">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="Enter Password"
+          required
+          className="w-full p-2 border border-gray-300 rounded-lg focus:ring focus:ring-blue-200 focus:border-blue-500"
+        />
+      </div>
+    </Modal>
   );
-}
+};
+
+export default UserForm;
