8e5dd424ff7c2357c27500116703cc71d3c5cbaa
poonyawat0931 <117950023+poonyawat0511@users.noreply.github.com>
2024-11-05 06:24:19 +0700

    frontend: Library: user search bar

--- frontend/admin/apps/library/src/app/users/page.tsx
+++ frontend/admin/apps/library/src/app/users/page.tsx
@@ -31,16 +31,13 @@ const fetchUsers = async (): Promise<User[]> => {
 
 export default function UserPage() {
   const { addAlert } = useGlobalContext();
+  const [searchQuery, setSearchQuery] = useState<string>("");
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [isFormOpen, setIsFormOpen] = useState<boolean>(false);
   const [isConfirmDialogOpen, setIsConfirmDialogOpen] = useState(false);
-  const [userIdToDelete, setUserIdToDelete] = useState<string | null>(
-    null
-  );
-  const [selectedUser, setSelectedUser] = useState<User | null>(
-    null
-  );
+  const [userIdToDelete, setUserIdToDelete] = useState<string | null>(null);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
   const handleAddAlert = (
     iconName: keyof typeof Icons,
     title: string,
@@ -85,7 +82,7 @@ export default function UserPage() {
 
   const handleDelete = async () => {
     if (!userIdToDelete) return;
-  
+
     try {
       const response = await fetch(`${apiUrl}/${userIdToDelete}`, {
         method: "DELETE",
@@ -94,7 +91,12 @@ export default function UserPage() {
         throw new Error("Failed to delete timeslot");
       }
       setUsers(users.filter((t) => t.id !== userIdToDelete));
-      handleAddAlert("ExclamationCircleIcon", "Success", "Transaction deleted successfully", tAlertType.SUCCESS);
+      handleAddAlert(
+        "ExclamationCircleIcon",
+        "Success",
+        "Transaction deleted successfully",
+        tAlertType.SUCCESS
+      );
     } catch (error) {
       console.log(error);
     } finally {
@@ -156,6 +158,17 @@ export default function UserPage() {
     }
   };
 
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredUsers = users.filter((user) => {
+    const matchesSearch = user.username
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase());
+    return matchesSearch;
+  });
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -164,9 +177,7 @@ export default function UserPage() {
     <div className="min-h-screen p-6">
       <div className="container mx-auto">
         <div className="flex justify-between items-center mb-4 px-4 border-b-2">
-          <h1 className="text-3xl font-bold mb-6 text-gray-800">
-            User
-          </h1>
+          <h1 className="text-3xl font-bold mb-6 text-gray-800">User</h1>
           <button
             onClick={handleCreate}
             className="bg-black text-white px-4 py-2 rounded-full shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-white mb-6"
@@ -174,13 +185,46 @@ export default function UserPage() {
             New User
           </button>
         </div>
-        <div className="flex flex-wrap justify-start">
-          <UserTable
-            users={users}
-            onEdit={handleEdit}
-            onDelete={confirmDelete}
-          />
+
+        <div className="flex items-center justify-between mb-6">
+          <div className="relative w-1/2">
+            <input
+              type="text"
+              placeholder="Search user here"
+              value={searchQuery}
+              onChange={handleSearchChange}
+              className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-gray-300"
+            />
+            <svg
+              className="w-5 h-5 text-gray-400 absolute left-3 top-1/2 transform -translate-y-1/2"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-4.35-4.35M9 17a8 8 0 100-16 8 8 0 000 16z"
+              />
+            </svg>
+          </div>
         </div>
+        
+        {filteredUsers.length > 0 ? (
+          <div className="flex flex-wrap justify-start">
+            <UserTable
+              users={filteredUsers}
+              onEdit={handleEdit}
+              onDelete={confirmDelete}
+            />
+          </div>
+        ) : (
+          <div className="text-center text-gray-500">
+            No users match your search.
+          </div>
+        )}
 
         {isFormOpen && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
@@ -198,7 +242,7 @@ export default function UserPage() {
         isOpen={isConfirmDialogOpen}
         onConfirm={handleDelete}
         onClose={() => setIsConfirmDialogOpen(false)}
-        message="Are you sure you want to delete this time slot?"
+        message="Are you sure you want to delete this user?"
       />
     </div>
   );
